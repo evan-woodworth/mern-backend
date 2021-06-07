a5c6fbb486726b3f0471b3c22595b35e051c509b
evan-woodworth <evan.woodworth@gmail.com>
2021-06-07 07:11:13 -0700

    add schema

--- models/User.js
+++ models/User.js
@@ -3,5 +3,30 @@ const { Schema } = mongoose;
 
 // User Schema
 
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    timesLoggedIn: {
+        type: Number,
+        default: 0
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
 const User = mongoose.model('User', userSchema);
 module.exports = User
